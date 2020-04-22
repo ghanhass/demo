0c00c5f553222af10b2039308a1d41016668d97e
hgh <hassen.ghanmi@vneuron.tn>
2020-04-22 23:54:58 +0100

    update

--- kyc_site/myjs/custom.js
+++ kyc_site/myjs/custom.js
@@ -1,20 +1,23 @@
-function postionHeader(headerElement, afterHeaderElement){
+function postionHeader(headerElement, afterHeaderElement, data){
     var topDist = afterHeaderElement.getBoundingClientRect().top;
     if(topDist < 0){
         headerElement.classList.add('fixed-header');
-        afterHeaderElement.classList.add('pt-174');
+        /*afterHeaderElement.classList.add('pt-174');*/
+        afterHeaderElement.style.paddingTop = (data.specificPaddingTop + 74)+"px"
     }
     else{
         headerElement.classList.remove('fixed-header');
-        afterHeaderElement.classList.remove('pt-174');
+        /*afterHeaderElement.classList.remove('pt-174');*/
+        afterHeaderElement.style.paddingTop = "";
     }
 }
 window.addEventListener("load", function(loadEvent){
     var headerElement = document.querySelector(".header-top");
     var afterHeaderElement = document.querySelector(".header-content") || headerElement.nextElementSibling;
-    postionHeader(headerElement, afterHeaderElement);
+    var specificPaddingTop = parseInt(window.getComputedStyle(afterHeaderElement).paddingTop);
+    postionHeader(headerElement, afterHeaderElement,{'specificPaddingTop': specificPaddingTop});
     window.addEventListener("scroll", function(event){
-        postionHeader(headerElement, afterHeaderElement);
+        postionHeader(headerElement, afterHeaderElement, {'specificPaddingTop': specificPaddingTop});
     });              
     //////
 
